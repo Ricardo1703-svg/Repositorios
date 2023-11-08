e85567d1d76cbafb1256ba73dd0c21809b6a4cec
Ricardo1703-svg <ralvares961@gmail.com>
2023-11-08 11:57:23 -0600

    Se agrego la app1

--- proyectoRpro/proyectoRpro/settings.py
+++ proyectoRpro/proyectoRpro/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'proyectoRpro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': BASE_DIR / 'db.sqlite3',
+        "USER" : "USUARIO",
+        "PASSWORD" : "Alvares17",
+        "HOST" : "NOM SERVER",
+        "PORT" : "5432"
+
     }
 }
 
