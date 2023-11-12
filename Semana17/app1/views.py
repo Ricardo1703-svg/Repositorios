5ccea09922e32ae21ae554e3474c8cb73b47aba0
Ricardo1703-svg <ralvares961@gmail.com>
2023-11-11 18:13:27 -0600

    Creo

--- Semana17/app1/views.py
+++ Semana17/app1/views.py
@@ -6,13 +6,13 @@ def lista_area(request):
     return render(request, 'area.html',{'Areas': Areas})
 
 def lista_cliente(request):
-    Clientes = Area.objects.all()
+    Clientes = Cliente.objects.all()
     return render(request, 'cliente.html',{'Clientes': Clientes})
 
 def lista_empleado(request):
-    Empleados = Area.objects.all()
+    Empleados = Empleado.objects.all()
     return render(request, 'empleado.html',{'Empleados': Empleados})
 
 def lista_venta(request):
-    Ventas = Area.objects.all()
+    Ventas = Venta.objects.all()
     return render(request, 'venta.html',{'Ventas': Ventas})
