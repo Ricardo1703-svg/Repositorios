e7abc5f3a6a5a6929799f933df7ad738c52fb27e
Ricardo1703-svg <ralvares961@gmail.com>
2024-01-18 11:55:11 -0600

    Terminado

---
+++ Practica1/Guia/temática libre/Scripts.js
@@ -0,0 +1,16 @@
+function mostrarmensajeMilan(){
+    alert("Jugador Promesa y Goleador Principal");
+    window.location.href = "https://www.acmilan.com/en/teams/men-first-team/all";
+}
+function mostrarmensajeBarca(){
+    alert("Delantero Consolidado");
+    window.location.href = "https://www.fcbarcelona.es/es/futbol/primer-equipo/jugadores";
+}
+function mostrarmensajeCity(){
+    alert("Guante de Oro y futuro de la seleccion");
+    window.location.href = "https://es.mancity.com/players/mens";
+}
+function mostrarmensajeMadrid(){
+    alert("Defensa Magistral");
+    window.location.href = "https://www.realmadrid.com/es-ES/futbol/primer-equipo-masculino/plantilla";
+}
