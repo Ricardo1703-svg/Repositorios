389784dcbdf6421f3fcdaf9a2e31c54c1e6d219f
Ricardo1703-svg <ralvares961@gmail.com>
2024-02-10 11:01:59 -0600

    Finalizado

--- Guía Laboratorio 2 – Semana 4/Scripts.js
+++ Guía Laboratorio 2 – Semana 4/Scripts.js
@@ -1,13 +1,13 @@
 // Mouseover
 document.getElementById('box1').addEventListener('mouseover', function() {
     this.style.backgroundColor = 'lightgreen';
-    document.getElementById('description1').innerText = "Este cuadro cambia de color cuando pasas el ratón sobre él.";
+    document.getElementById('description1').innerText = "Es una función en la que un usuario mueve el cursor sobre un elemento y produce una respuesta.";
 });
 
 // Click
 document.getElementById('box2').addEventListener('click', function() {
     this.style.backgroundColor = 'lightcoral';
-    document.getElementById('description2').innerText = "Este cuadro cambia de color cuando haces clic en él.";
+    document.getElementById('description2').innerText = "Es una función en la que un usuario hace clic en él y produce una respuesta.";
 });
 
 // Focus
