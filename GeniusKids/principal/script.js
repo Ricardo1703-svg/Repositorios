b9e0841d548b4425ef34ea42081e0935cd8fbf73
Ricardo1703-svg <ralvares961@gmail.com>
2024-07-05 11:41:26 -0600

    nada mas

--- GeniusKids/principal/script.js
+++ GeniusKids/principal/script.js
@@ -1,3 +1,7 @@
 function Video1(){
     window.location.href = "Video1.html"
 }
+
+function Video2(){
+    window.location.href = "https://hackernoon.com/es/los-24-mejores-css-free-to-use-buttons-with-codepen-examples"
+}
